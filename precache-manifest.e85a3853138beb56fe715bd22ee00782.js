f5cde6ab6df1f72d222c6758b885d4e247f47f3c
shahedex <shahed739@gmail.com>
2019-11-13 11:58:07 +0600

    added: all build files

---
+++ precache-manifest.e85a3853138beb56fe715bd22ee00782.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57338053e57fea16da8c4013dc05e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4769e500252e10d52f",
+    "url": "/static/css/2.794b9027.chunk.css"
+  },
+  {
+    "revision": "eb8ae75e4fed7be43ed2",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "ff4769e500252e10d52f",
+    "url": "/static/js/2.9447cbf4.chunk.js"
+  },
+  {
+    "revision": "eb8ae75e4fed7be43ed2",
+    "url": "/static/js/main.fe27a2a8.chunk.js"
+  },
+  {
+    "revision": "ff1355ebb3518d98870d",
+    "url": "/static/js/runtime-main.14445964.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/static/media/icons-20.90247cfd.ttf"
+  }
+]);
